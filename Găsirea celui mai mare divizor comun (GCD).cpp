c23f0e92b30e8ba71b80a3c5b37f8439a16d5fb6
Voicu Librimir <voicu.librimir@gmail.com>
2024-04-16 22:03:41 +0300

    Add project files.

---
+++ Găsirea celui mai mare divizor comun (GCD).cpp
@@ -0,0 +1,31 @@
+﻿// Găsirea celui mai mare divizor comun (GCD).cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int numar1, numar2;
+
+    // Citim cele două numere de la utilizator
+    std::cout << "Introduceti primul numar: ";
+    std::cin >> numar1;
+    std::cout << "Introduceti al doilea numar: ";
+    std::cin >> numar2;
+
+    // Calculăm GCD folosind algoritmul lui Euclid
+    int rezultat = gcd(numar1, numar2);
+
+    // Afișăm rezultatul
+    std::cout << "Cel mai mare divizor comun al lui " << numar1 << " si " << numar2 << " este: " << rezultat << std::endl;
+
+    return 0;
+}
